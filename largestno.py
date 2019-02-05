d9fb88c4b2c073ff8abcd421374edb9a0efd3c16
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-02-05 12:00:46 +0530

    Update largestno.py

--- largestno.py
+++ largestno.py
@@ -1,6 +1,6 @@
-n1=float(input("Enter 1st number: "))
-n2=float(input("Enter 2nd number: "))
-n3=float(input("Enter 3rd number: "))
+n1=float(input())
+n2=float(input())
+n3=float(input())
 if n1>n2 and n1>n3:
     print(n1)
 elif n2>n1 and n2>n3:
